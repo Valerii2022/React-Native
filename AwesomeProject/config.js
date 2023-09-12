0d3195c7e0effdae0db4b52c145cb7215e0047f3
Valerii Pometun <vmpometun@gmail.com>
2023-09-12 09:29:25 +0300

    add android app

--- AwesomeProject/config.js
+++ AwesomeProject/config.js
@@ -7,15 +7,25 @@ import { getFirestore } from "firebase/firestore";
 // Функція для підключення сховища файлів в проект
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAvhOkabYQuydlzAbrzrgvpfBV1Z8gP1Tk",
+//   authDomain: "final-pfoject.firebaseapp.com",
+//   databaseURL: "https://final-pfoject.firebaseio.com",
+//   projectId: "final-pfoject",
+//   storageBucket: "final-pfoject.appspot.com",
+//   messagingSenderId: "535838152318",
+//   appId: "1:535838152318:web:2688975729dd69044baa2f",
+//   measurementId: "G-2GQCKBTVNN",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAvhOkabYQuydlzAbrzrgvpfBV1Z8gP1Tk",
-  authDomain: "final-pfoject.firebaseapp.com",
-  databaseURL: "https://final-pfoject.firebaseio.com",
-  projectId: "final-pfoject",
-  storageBucket: "final-pfoject.appspot.com",
-  messagingSenderId: "535838152318",
-  appId: "1:535838152318:web:2688975729dd69044baa2f",
-  measurementId: "G-2GQCKBTVNN",
+  apiKey: "AIzaSyDaFWgu6iX3RFnnRD7nDksa8jFYjTgeVC0",
+  authDomain: "final-41e45.firebaseapp.com",
+  projectId: "final-41e45",
+  storageBucket: "final-41e45.appspot.com",
+  messagingSenderId: "306078631295",
+  appId: "1:306078631295:android:99693f35a665bdd63b864a",
+  // measurementId: "G-LSH850ZBWW",
 };
 
 const app = initializeApp(firebaseConfig);
